9a8181480afce142e35ca2e3ae54cd72cd5185a4
JessiiPer <jessica_roch@hotmail.com>
2020-01-17 10:40:21 -0300

    Quebrando dominio em serviços

---
+++ src/main/java/br/com/alura/microservice/fornecedor/model/InfoFornecedor.java
@@ -0,0 +1,50 @@
+package br.com.alura.microservice.fornecedor.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class InfoFornecedor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String endereco;
+
+	private String estado;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+}
